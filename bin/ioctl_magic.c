2948d2f26faea7f18ab2558c9073dd6fd94c832e
Александр Мишин <mishin@mh.net.ru>
2021-09-20 23:20:14 +0400

    To avoid printf() with only one argument #3

--- bin/ioctl_magic.c
+++ bin/ioctl_magic.c
@@ -7,8 +7,8 @@
 int
 main()
 {
-    printf("RCRECV_READ_CODE      int:%lu hex:0x%lx size:%d\n", RCRECV_READ_CODE, RCRECV_READ_CODE, sizeof(unsigned long));
-    printf("RCRECV_READ_CODE_INFO int:%lu hex:0x%lx size:%d\n", RCRECV_READ_CODE_INFO, RCRECV_READ_CODE_INFO, sizeof(struct rcrecv_code));
+    printf("RCRECV_READ_CODE      int:%lu hex:0x%lx size:%u\n", RCRECV_READ_CODE, RCRECV_READ_CODE, sizeof(unsigned long));
+    printf("RCRECV_READ_CODE_INFO int:%lu hex:0x%lx size:%u\n", RCRECV_READ_CODE_INFO, RCRECV_READ_CODE_INFO, sizeof(struct rcrecv_code));
 
     return (0);
 }
